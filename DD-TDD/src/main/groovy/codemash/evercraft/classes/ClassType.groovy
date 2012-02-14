69d384d384c260b1fe61eda078d78dec1593e120
Mike Cornell <mcornell74@gmail.com>
2012-02-14 07:44:13 -0500

    Adding War Monk

--- DD-TDD/src/main/groovy/codemash/evercraft/classes/ClassType.groovy
+++ DD-TDD/src/main/groovy/codemash/evercraft/classes/ClassType.groovy
@@ -3,12 +3,14 @@ package codemash.evercraft.classes
 import codemash.evercraft.character.Character
 
 enum ClassType {
-	FIGHTER, ROGUE, PEASANT
+	FIGHTER, ROGUE, WAR_MONK, PEASANT
 	
 	def hitPointsPerLevel() {
 		switch (this) {
 			case FIGHTER:
 				return 10;
+			case WAR_MONK:
+				return 6;
 			default:
 				return 5;
 		}
